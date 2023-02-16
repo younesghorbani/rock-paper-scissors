1c8c651afe9f20b5a77d79407e02dd5e914ab4bb
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-02-16 19:54:22 +0330

    Edit script.js
    Added playRound function. In this function,
    the option selected by the player and the computer are compared
    with each other, and depending on the selected options,
    one of the players is selected as the winner and the other as the loser.
    Also, if the player's input is in uppercase or lowercase letters,
    or a combination of both, using the "capitalize" helper function,
    the first letter is converted to uppercase to match
    the option selected by the computer.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 function getComputerChoice() {
-    let choice = Math.floor((Math.random() * 3));
+    let choice = Math.floor(Math.random() * 3);
 
     switch (choice) {
         case 0:
-            return 'rock';
+            return 'Rock';
             break;
         case 1:
-            return 'paper';
+            return 'Paper';
             break;
         case 2:
-            return 'scissors';
+            return 'Scissors';
             break;
     }
 }
+
+function capitalize(word) {
+    return word.slice(0, 1).toUpperCase().concat(word.slice(1).toLowerCase());
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (capitalize(playerSelection).localeCompare(computerSelection) == 0) {
+        return;
+    } else if (capitalize(playerSelection) === 'Rock' && computerSelection === 'Scissors') {
+        return `You win! ${capitalize(playerSelection)} beats ${computerSelection}.`
+    } else if (capitalize(playerSelection) === 'Scissors' && computerSelection === 'Paper') {
+        return `You win! ${capitalize(playerSelection)} beats ${computerSelection}.`
+    } else if (capitalize(playerSelection) === 'Paper' && computerSelection === 'Rock') {
+        return `You win! ${capitalize(playerSelection)} beats ${computerSelection}.`
+    } else {
+        return `You lose! ${computerSelection} beats ${capitalize(playerSelection)}.`
+    }
+}
+
+const playerSelection = prompt('Choose one. "Rock", "Paper" or "Scissors" (Case Insensitive)', 'Rock');
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
