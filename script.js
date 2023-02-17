fc48b831bb70c77a99795a6b35c0eb0801b836b8
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-02-17 15:47:20 +0330

    Edit script.js
    I realized that the output of the program was not correct, so
    I edited the "game" function. Before this, game was played in 5 rounds
    and the winner was the player who got the most points.
    But this is not correct. Now the game is played in infinite rounds
    and whichever of the two players (user or computer) reaches
    five points first is the winner. Also, some parts of this function
    were changed to make the output look cleaner.

--- script.js
+++ script.js
@@ -20,54 +20,48 @@ function capitalize(word) {
 
 function playRound(playerSelection, computerSelection) {
     if (capitalize(playerSelection).localeCompare(computerSelection) === 0) {
-        return `Both of you chose ${computerSelection}. It is a tie!`;
+        return `It's a tie! Both of you chose ${computerSelection}.`;
     } else if (capitalize(playerSelection) === 'Rock' && computerSelection === 'Scissors') {
-        return `You won this round! ${capitalize(playerSelection)} beats ${computerSelection}.`;
+        return `You won! ${capitalize(playerSelection)} beats ${computerSelection}.`;
     } else if (capitalize(playerSelection) === 'Scissors' && computerSelection === 'Paper') {
-        return `You won this round! ${capitalize(playerSelection)} beats ${computerSelection}.`;
+        return `You won! ${capitalize(playerSelection)} beats ${computerSelection}.`;
     } else if (capitalize(playerSelection) === 'Paper' && computerSelection === 'Rock') {
-        return `You won this round! ${capitalize(playerSelection)} beats ${computerSelection}.`;
+        return `You won! ${capitalize(playerSelection)} beats ${computerSelection}.`;
     } else {
-        return `You lost this round! ${computerSelection} beats ${capitalize(playerSelection)}.`;
+        return `You lost! ${computerSelection} beats ${capitalize(playerSelection)}.`;
     }
 }
 
 function game() {
     let playerScore = computerScore = 0;
+    let round = 0;
     let result;
 
-    for (let round = 0; round < 5; round++) {
+    while (playerScore < 5 && computerScore < 5) {
+        round++;
+
         const playerSelection = prompt('Choose one. "Rock", "Paper" or "Scissors" (Case Insensitive)', 'Rock');
         const computerSelection = getComputerChoice();
         
         result = playRound(playerSelection, computerSelection);
-        console.log(result);
 
-        if (result.includes('won')) {
-            playerScore++;
-        } else if (result.includes('lost')) {
-            computerScore++;
-        } else {
-            continue;
-        }
+        if (result.includes('won')) playerScore++;
+        if (result.includes('lost')) computerScore++;
+
+        console.log(`Round #${round}: ${result}`);
+        console.log(`You ${playerScore} - ${computerScore} Computer`);
     }
+
+    console.log('Final result:');
 
     if (playerScore > computerScore) {
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${computerScore}`);
-        console.log('Hooray! You are the winner :)');
-    }
-    
-    if (playerScore < computerScore) {
+        console.log('Hooray! You are the WINNER :)');
+    } else {
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${computerScore}`);
-        console.log('Sorry! You lost :(');
-    }
-    
-    if (playerScore === computerScore) {
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer score: ${computerScore}`);
-        console.log('We do not have a winner! Maybe next time ;)');
+        console.log('Sorry! You LOST :(');
     }
 }
 
